1f2db57b9ef4ce3b6772b364c19c05c9e604fdfc
Rich Bonny <RichBonny@hotmail.com>
2015-05-26 00:10:16 +0000

    Added notice and styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   def save_with_payment
     if valid?
+      Stripe.api_key = "sk_test_jB9WCuaW403H2UDCrc9lkSXF"
       customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
